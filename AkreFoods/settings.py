8ba0dbdbba2dc9b6e3edaeb5623c2d6175da6fea
chimaroke onyebi <maroks216@gmail.com>
2023-06-23 04:09:36 +0100

    Register user function

--- AkreFoods/settings.py
+++ AkreFoods/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# detect message tags success or error
+MESSAGE_TAGS = {
+        messages.ERROR: 'danger',
+}
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import *
+from django.contrib import messages
 
 
 
 
 
 def registerUser(request):
-    pass
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password'] #Hashing the password 
+            user = form.save(commit=False)
+            user.set_password(password)
+            user.role = User.CUSTOMER
+            user.save()
+            messages.success(request, 'Registration Successful')
+            return redirect('register-user')
+        else:
+            print(form.errors)
+    else:
+        form = UserForm()
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
