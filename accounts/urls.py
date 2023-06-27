f42a1c47084ae0a7ba37a4960ae981a5e034616b
chimaroke onyebi <maroks216@gmail.com>
2023-06-27 17:44:45 +0100

    customer and vendor registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('registerUser/', views.registerUser, name='register-user'),
-    path('registerVendor/', views.registerVendor, name='register-vendor')
+    path('registerVendor/', views.registerVendor, name='register-vendor'),
+    
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutPage, name='logout'),
+    path('myAccount/', views.myAccount, name='my-account'),
+    path('customerDashboard/', views.customerDashboard, name='customer-dashboard'),
+    path('vendorDashboard/', views.vendorDashboard, name='vendor-dashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render,redirect
 from .forms import *
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required, user_passes_test # activates custom decorators
 from vendor.forms import *
 from .models import *
+from .utils import *
+from django.core.exceptions import PermissionDenied
+
+# Restrict the vendor from accessing the customer page
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
 
 
+# Restrict the customer from accessing the vendor page
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
 
 
 
 def registerUser(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request, 'Already logged in')
+        return redirect('dashboard')
+    elif request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
             password = form.cleaned_data['password'] #Hashing the password 
@@ -54,3 +73,47 @@ def registerVendor(request):
         'v_form':v_form,
     }
     return render(request, 'accounts/register_vendor.html', context)
+
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        messages.warning(request, 'Already logged in')
+        return redirect('dashboard')
+    elif request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        # Returns which user owns the email and password
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)   
+            messages.success(request, 'Login Successful')
+            return redirect('my-account')
+        else:
+            messages.error(request, 'Invalid Input')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+
+def logoutPage(request):
+    auth.logout(request)
+    messages.info(request, 'Logged out')
+    return redirect('login')
+
+@login_required(login_url='login')
+def myAccount(request):
+    user = request.user
+    redirectUrl = detectUser(user)
+    return redirect(redirectUrl)
+
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def customerDashboard(request):
+    return render(request, 'accounts/customer_dashboard.html')
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def vendorDashboard(request):
+    return render(request, 'accounts/vendor_dashboard.html')
